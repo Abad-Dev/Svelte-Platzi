0df90fa6f891274cdae27474e0789b92f9eef5b1
sedron <jfabianabadd@gmail.com>
2022-06-08 19:02:08 -0500

    Created Navbar & routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,33 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import { Route, Router } from 'svelte-navigator';
+  import Nav from './lib/Nav.svelte'
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+  <Router>
 
-  <Counter />
+    <Nav />
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
+    <Route path="/">
+      <h1>Hola mundo</h1>
+    </Route>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+    <Route path="/empresa">
+      <h2>Esta es la empresa</h2>
+    </Route>
+
+  </Router>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  :global(html){
+    border-top: 10px solid rgb(111, 184, 61);
+    min-height: 100%;
   }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+  :global(.humanase-text){
+    color: rgb(111, 184, 61);
   }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  :global(.humanase-hover:hover){
+    color: rgb(111, 184, 61);
   }
 </style>
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { Link } from 'svelte-navigator';
+    import logo from './logo.jpg';
+  </script>
+  
+
+<main>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container">
+            <Link to="/" class="navbar-brand d-flex" draggable="false">
+                <img src={logo} alt="" draggable="false">
+                <div>
+                    <h1 class="humanase-text mt-2 mb-0">Humana</h1>
+                    <small style="font-size: 13px; float: right;" class="text-muted fw-bold">Socios Estratégicos</small>
+                </div>
+            </Link>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <Link to="/" class="nav-link humanase-hover">Home</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/empresa" class="nav-link humanase-hover">La empresa</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/servicios" class="nav-link humanase-hover">Servicios</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/clientes" class="nav-link humanase-hover">Clientes </Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/contactenos" class="nav-link humanase-hover">Contáctenos</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</main>
